73fb14fd91a5cd56b3e0a2ee585afa17dfbf2895
Lakshmanan Palaniappan <154072727+Lakshmanan-palaniappan@users.noreply.github.com>
2026-02-14 20:52:56 +0530

    Revert "Intern Registration"
    This reverts commit d47e908846f85795577d93e19ed893c9806efbc0.

--- Register.py
+++ Register.py
@@ -1,8 +1,9 @@
 def register(user):
     if user=="Admin":
         print("Admin Registered")
-    elif user=="Intern":
-        print("Intern Registered")
+
+    
     elif user=="Associate":
         print("Associate Registered")
+
     
