1b97c64f62b96b5467509f585d590b00c66db1c9
Lakshmanan Palaniappan <lakshmananletchu8@gmail.com>
2026-02-17 09:53:30 +0530

    feat: Tested Hooks

--- src/generators/gen_size.py
+++ src/generators/gen_size.py
@@ -7,5 +7,6 @@ gen_list=(num for num in range(1,10000000000))
 #print("Normal List Size: ", sys.getsizeof(normal_list))
 print("Generator List Size: ", sys.getsizeof(gen_list))
 
-
+print("This is a Generator List")
 print(list(itertools.islice(gen_list, 100)))
+    
